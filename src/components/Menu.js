6c8d3e854267481654aa826de3714dc058a92b36
CamilleLecoq <55230689+EstebanEpitech@users.noreply.github.com>
2023-12-12 14:20:52 +0100

    [~] Return to the main page when not log in final form

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,32 +1,27 @@
-import React, { useContext, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import { AuthContext } from './AuthContext';
 import logo from '../assets/logo.png';
 import profile from '../assets/profile.png';
 
+
 const Menu = () => {
   const { isLoggedIn } = useContext(AuthContext);
-  const navigate = useNavigate(); // Hook pour la navigation
-
-  useEffect(() => {
-    if (!isLoggedIn) {
-      navigate('/'); // Redirige vers la page d'accueil si l'utilisateur n'est pas connecté
-    }
-  }, [isLoggedIn, navigate]); // Dépendances de useEffect
 
   return (
     <nav className="bg-gray-800 p-4 flex justify-between items-center fixed w-full">
       <div className="flex items-center">
+        
         <Link to="/"><img src={logo} alt="Logo" className="h-12 w-12 mr-2" /></Link>
-        <Link to="/page1" className="text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700">Page 1</Link>
+        <Link to="/page1" className="text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700">Page SCD</Link>
         <Link to="/page2" className="text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700">Page 2</Link>
         <Link to="/page3" className="text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700">Page 3</Link>
-        {isLoggedIn && <Link to="/admin" className="text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700">Admin Dashboard</Link>}
+        <Link to="/admin" className="text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700">Admin Dashboard</Link>
       </div>
       <div className="flex items-center">
-        {!isLoggedIn && <Link to="/login" className="text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700">Se connecter</Link>}
+      {!isLoggedIn && <Link to="/login" className="text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700">Se connecter</Link>}
         {isLoggedIn && <Link to="/profile" className="ml-4 flex items-center justify-center bg-gray-700 text-white rounded-full h-10 w-10">
-          <img src={profile} alt="Profile" className="p-1 h-full w-full" />
+            <img src={profile} alt="Profile" className="p-1 h-full w-full" />
         </Link>}
       </div>
     </nav>
--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,26 +1,29 @@
-import React, { useContext } from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import React, { useContext, useEffect } from 'react';
+import { Link, Outlet, useNavigate } from 'react-router-dom';
 import { AuthContext } from './AuthContext';
 
+const SideMenu = () => {
+  const { isLoggedIn } = useContext(AuthContext);
+  const navigate = useNavigate(); // Hook pour la navigation
 
+  useEffect(() => {
+    if (!isLoggedIn) {
+      navigate('/'); // Redirige vers la page d'accueil si l'utilisateur n'est pas connecté
+    }
+  }, [isLoggedIn, navigate]); // Dépendances de useEffect
 
-const Menu = () => {
-    const jwtToken = localStorage.getItem('JWToken');
-  
-    return (
-        <div className="flex pt-20">
-            <nav className="bg-gray-800 p-4 flex flex-col fixed h-full">
-                <Link to="/admin/top100" className="text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700">Top100</Link>
-                <Link to="/admin/users" className="text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700">User Manager</Link>
-                <Link to="/admin" className="text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700">Page 3</Link>
-            </nav>
-            <div className="content ml-[12vw] w-full mr-4">
-                <Outlet />
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex pt-20">
+      <nav className="bg-gray-800 p-4 flex flex-col fixed h-full">
+        <Link to="/admin/top100" className="text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700">Top100</Link>
+        <Link to="/admin/users" className="text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700">User Manager</Link>
+        <Link to="/admin" className="text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700">Page 3</Link>
+      </nav>
+      <div className="content ml-[12vw] w-full mr-4">
+        <Outlet />
+      </div>
+    </div>
+  );
 };
-  
-export default Menu;
 
-  
+export default SideMenu;
